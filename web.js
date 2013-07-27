436fe27db64dab6f84ce90bd3062c1605c9f3af6
Antonio F. Lancho <onioman@gmail.com>
2013-07-27 10:19:17 +0900

    Re-read index file with every reload.

--- web.js
+++ web.js
@@ -4,8 +4,13 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var indexContent = fs.readFileSync('index.html', 'utf8');
 
+var getIndexContent = function(index) {
+	return fs.readFileSync(index, 'utf8');
+};
+
 app.get('/', function(request, response) {
-  response.send(indexContent.toString("utf-8"));
+	//response.send(indexContent.toString("utf-8"));
+	response.send(getIndexContent('index.html').toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
